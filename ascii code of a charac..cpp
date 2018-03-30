a6005f1feb1059b27615cd180b9b9278c7c10059
yash kabra <yashkabra007.yk@gmail.com>
2018-03-30 19:06:32 +0530

    1 new

---
+++ ascii code of a charac..cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+cout<<"enter a character\n";
+char ch;
+cin>>ch;
+int num =ch;
+cout<<"the ascii value of this character is\n"<<num<<"\n";
+
+}
